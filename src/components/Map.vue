65b4f2c0edac3a5fa205729ea7763ccb97df8b6e
ave10987 <ave10987@naver.com>
2018-07-25 00:24:18 +0900

    map update

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,23 +1,9 @@
 <template>
-  <div>
-    <div id="map" style="width:100%;height:200px;"></div>
-    <a href="tmap://?rGoName=브라이드밸리&rGoX=127.033433&rGoY=37.486922">T Map으로 경로안내</a>
-    <a href="#" @click="goKakaoNavi()">KAKAO NAVI로 경로안내</a>
-  </div>
+  <div id="map" style="width:100%;height:40%"></div>
 </template>
 <script>
 export default {
   name: 'Map',
-  methods: {
-    goKakaoNavi() {
-      Kakao.Navi.start({
-        name: "브라이드밸리",
-        x: 127.033433,
-        y: 37.486922,
-        coordType: 'wgs84'
-     });
-    }
-  },
   mounted() {
     const center = new window.naver.maps.LatLng(37.486922, 127.033433);
     const map = new window.naver.maps.Map('map', {
